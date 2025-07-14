bfb8372b06f227b9629270e887f0583327c0d637
Iestyn Jones <iestyn02@gmail.com>
2025-07-14 18:41:18 +0100

    Properly setup .env retrieval for server (deployment

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,18 @@
 import { app } from './app.js';
 
+import path from 'path';
+
+import { fileURLToPath } from 'url';
+
+import dotenv from 'dotenv';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
+
+console.info(process.env);
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
