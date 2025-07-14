44033c2a0b9fe3f9132a0cc6dbe1ee1089e11b3d
Iestyn Jones <iestyn02@gmail.com>
2025-07-14 19:33:32 +0100

    Updated ecoystem.config.cjs

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "server",
-      script: "./server.js",
+      script: "/var/api/nasa.codebrew.cc/backend/server.js",
       cwd: "/var/api/nasa.codebrew.cc/backend",
       interpreter: "node",
       node_args: "--enable-source-maps",
